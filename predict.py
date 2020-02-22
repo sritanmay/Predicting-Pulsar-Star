279be1a861266235ddff1cfeb5acebb7511a38d0
sritanmay <tanmayasrivastava99@gmail.com>
2020-02-22 17:36:36 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,37 @@
+# Kernel SVM
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('pulsar_stars.csv')
+X = dataset.iloc[:, 0:8].values
+y = dataset.iloc[:, 8].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =1/5, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Kernel SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+from sklearn.metrics import mean_absolute_error
+error = mean_absolute_error(y_test, y_pred)
+
